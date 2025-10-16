fd3e9b85a6251674a1517669398de825be6db286
aikins23 <buabengemmanuelaikins@gmail.com>
2025-10-16 15:42:31 +0000

    Initial commit

--- task_4.sql
+++ task_4.sql
@@ -2,5 +2,7 @@ USE alx_book_store;
 -- Print full description of the table books from alx_book_store
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_SCHEMA = DATABASE()
-AND TABLE_NAME = 'books';
+WHERE TABLE_SCHEMA = 'alx_book_store'
+AND TABLE_NAME = 'Books';
+
+
