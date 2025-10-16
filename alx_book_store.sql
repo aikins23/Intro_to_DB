f125b7905cc753d8021eed6955d613e3ad0d0917
aikins23 <buabengemmanuelaikins@gmail.com>
2025-10-16 14:42:08 +0000

    Initial commit

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -30,7 +30,7 @@ CREATE TABLE IF NOT EXISTS Orders(
 order_id INT PRIMARY KEY AUTO_INCREMENT,
 customer_id INT,
 order_date DATE,
-FOREIGN KEY (customer_id ) REFERENCES Customers(customer_id )
+FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 
 );
 
